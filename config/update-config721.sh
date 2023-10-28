ccb320c6f6754218fa35148e499934989883be0f
Peter Suh <85427533+PeterSuh-Q3@users.noreply.github.com>
2023-10-28 19:40:37 +0900

    Update update-config721.sh

--- config/update-config721.sh
+++ config/update-config721.sh
@@ -1,4 +1,4 @@
-for model in `cat models`
+for model in `cat models.72`
 do 
 echo "modify config.json on $model"
 value=`grep $model _common/kernel-bs-fb-patch-for-all/files-chksum | grep os.sha256|awk '{print $1}'`
@@ -22,11 +22,11 @@ sed -i 's#release/7.2/69057-1#release/7.2.1/69057#g' $model/7.2.1-69057/config.j
 
 done 
 
-for model in `cat models`
+for model in `cat models.72`
 do 
 echo "rename bsp file on $model"
 remodel=$(echo "$model" | sed 's/DS//' | sed 's/RS//' | sed 's/+/p/' | sed 's/DVA/dva/' | sed 's/FS/fs/' | sed 's/SA/sa/' )
 echo "$remodel"
-cp _common/kernel-bs-fb-patch-for-all/zImage-001-${model}-69057-ramdisk-and-flag-NOP.bsp $model/7.2.1-69057/zImage-001-${remodel}-69057-ramdisk-and-flag-NOP.bsp
+#cp _common/kernel-bs-fb-patch-for-all/zImage-001-${model}-69057-ramdisk-and-flag-NOP.bsp $model/7.2.1-69057/zImage-001-${remodel}-69057-ramdisk-and-flag-NOP.bsp
 
 done
