9841b0b8270d1a33f03135072581525c523d8eb8
Peter Suh <85427533+PeterSuh-Q3@users.noreply.github.com>
2023-12-02 12:36:59 +0900

    Update file.sh

--- include/file.sh
+++ include/file.sh
@@ -291,7 +291,7 @@ brp_cp_from_list()
 # Args: $1 URL to download from | $2 destination file | $3 hard fail on error [1 to do so]
 rpt_download_remote()
 {
-    if [ $(echo "${1}" | grep tcrp-addons | wc -l) -gt 0 ]; then
+    if [ -d /dev/shm/tcrp-addons/ ]||[ -d /dev/shm/tcrp-modules/ ]; then
       fromfile=$(echo "${1}" | sed 's#https://raw.githubusercontent.com/PeterSuh-Q3#/dev/shm#' | sed 's#/master##' | sed 's#/main##')
       pr_info "Copy downloaded file %s to %s" "${fromfile}" "${2}"
       cp -f "${fromfile}" "${2}"
